d8af5c2bbfd8925bb615b364fbb7b912593e6ab6
Anthony PACIONE <antho.pacione@gmail.com>
2022-02-28 18:08:20 +0100

    ajout entities Devis, Reservation, Trajet

---
+++ src/main/java/com/inti/entities/Reservation.java
@@ -0,0 +1,174 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Reservation implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idResa;
+	@Temporal(TemporalType.DATE)
+	private Date dateCreation;
+	@Temporal(TemporalType.DATE)
+	private Date dateValidation;
+	private String statut;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "reservation", joinColumns = @JoinColumn(name = "id_reservation", referencedColumnName = "idTrajet"), inverseJoinColumns = @JoinColumn(name = "id_trajet", referencedColumnName = "idTrajet"))
+	private Set<Trajet> trajets = new HashSet<>();
+
+	@OneToOne
+	@JoinColumn(name = "id_devis")
+	private Devis devis;
+
+	@OneToOne
+	@JoinColumn(name = "id_facture")
+	private Facture facture;
+
+	@OneToOne
+	@JoinColumn(name = "id_avisClient")
+	private AvisClient avisClient;
+
+	@OneToMany(mappedBy = "reservation")
+	List<Reclamation> reclamations = new ArrayList<>();
+
+	@ManyToOne
+	@JoinColumn(name = "id_Client")
+	private Client client;
+
+	@ManyToOne
+	@JoinColumn(name = "id_chauffeur")
+	private Chauffeur chauffeur;
+
+	public Reservation() {
+	}
+
+	public Reservation(Date dateCreation, Date dateValidation, String statut, Set<Trajet> trajets, Devis devis,
+			Facture facture, AvisClient avisClient, List<Reclamation> reclamations, Client client,
+			Chauffeur chauffeur) {
+		this.dateCreation = dateCreation;
+		this.dateValidation = dateValidation;
+		this.statut = statut;
+		this.trajets = trajets;
+		this.devis = devis;
+		this.facture = facture;
+		this.avisClient = avisClient;
+		this.reclamations = reclamations;
+		this.client = client;
+		this.chauffeur = chauffeur;
+	}
+
+	public Long getIdResa() {
+		return idResa;
+	}
+
+	public void setIdResa(Long idResa) {
+		this.idResa = idResa;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	public Date getDateValidation() {
+		return dateValidation;
+	}
+
+	public void setDateValidation(Date dateValidation) {
+		this.dateValidation = dateValidation;
+	}
+
+	public String getStatut() {
+		return statut;
+	}
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+
+	public Set<Trajet> getTrajets() {
+		return trajets;
+	}
+
+	public void setTrajets(Set<Trajet> trajets) {
+		this.trajets = trajets;
+	}
+
+	public Devis getDevis() {
+		return devis;
+	}
+
+	public void setDevis(Devis devis) {
+		this.devis = devis;
+	}
+
+	public Facture getFacture() {
+		return facture;
+	}
+
+	public void setFacture(Facture facture) {
+		this.facture = facture;
+	}
+
+	public AvisClient getAvisClient() {
+		return avisClient;
+	}
+
+	public void setAvisClient(AvisClient avisClient) {
+		this.avisClient = avisClient;
+	}
+
+	public List<Reclamation> getReclamations() {
+		return reclamations;
+	}
+
+	public void setReclamations(List<Reclamation> reclamations) {
+		this.reclamations = reclamations;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Chauffeur getChauffeur() {
+		return chauffeur;
+	}
+
+	public void setChauffeur(Chauffeur chauffeur) {
+		this.chauffeur = chauffeur;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [idResa=" + idResa + ", dateCreation=" + dateCreation + ", dateValidation=" + dateValidation
+				+ ", statut=" + statut + ", trajets=" + trajets + ", devis=" + devis + "]";
+	}
+
+}
---
+++ src/main/java/com/inti/entities/Trajet.java
@@ -0,0 +1,101 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Trajet implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idTrajet;
+	private String adresseDep;
+	private String adresseArr;
+	private double distance;
+	private float duree;
+	@Temporal(TemporalType.DATE)
+	private Date dateDep;
+	@Temporal(TemporalType.DATE)
+	private Date dateArr;
+
+	public Trajet() {
+	}
+
+	public Trajet(String adresseDep, String adresseArr, double distance, float duree, Date dateDep, Date dateArr) {
+		this.adresseDep = adresseDep;
+		this.adresseArr = adresseArr;
+		this.distance = distance;
+		this.duree = duree;
+		this.dateDep = dateDep;
+		this.dateArr = dateArr;
+	}
+
+	public Long getIdTrajet() {
+		return idTrajet;
+	}
+
+	public void setIdTrajet(Long idTrajet) {
+		this.idTrajet = idTrajet;
+	}
+
+	public String getAdresseDep() {
+		return adresseDep;
+	}
+
+	public void setAdresseDep(String adresseDep) {
+		this.adresseDep = adresseDep;
+	}
+
+	public String getAdresseArr() {
+		return adresseArr;
+	}
+
+	public void setAdresseArr(String adresseArr) {
+		this.adresseArr = adresseArr;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+	public float getDuree() {
+		return duree;
+	}
+
+	public void setDuree(float duree) {
+		this.duree = duree;
+	}
+
+	public Date getDateDep() {
+		return dateDep;
+	}
+
+	public void setDateDep(Date dateDep) {
+		this.dateDep = dateDep;
+	}
+
+	public Date getDateArr() {
+		return dateArr;
+	}
+
+	public void setDateArr(Date dateArr) {
+		this.dateArr = dateArr;
+	}
+
+	@Override
+	public String toString() {
+		return "Trajet [idTrajet=" + idTrajet + ", adresseDep=" + adresseDep + ", adresseArr=" + adresseArr
+				+ ", distance=" + distance + ", duree=" + duree + ", dateDep=" + dateDep + ", dateArr=" + dateArr + "]";
+	}
+
+}
