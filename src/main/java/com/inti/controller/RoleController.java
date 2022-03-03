b20199803b21a7452cee2cc8b24f228ce22e1c38
MaevaSouchu <maeva.souchou@gmail.com>
2022-03-03 16:01:36 +0100

    Ajout role controller et modif de administrateur controller

--- src/main/java/com/inti/controller/RoleController.java
+++ src/main/java/com/inti/controller/RoleController.java
@@ -1,9 +1,20 @@
 package com.inti.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.inti.entities.Role;
 import com.inti.service.interfaces.IRoleService;
 
 @RestController
@@ -12,4 +23,35 @@ public class RoleController {
 
 	@Autowired
 	IRoleService roleService;
+	
+	@GetMapping("roles") // equivalent à requestMapping
+	public List<Role> findAll(){
+		return roleService.findAll();
+	}
+	
+	@GetMapping("roles/{idR}") //localhost:8080/roles/6 
+	// @RequestMapping(value="roles/{idR}", method=RequestMethod.GET)
+	public Role findOne(@PathVariable("idR")Long idRole) { // idRole=6
+		return roleService.findOne(idRole); //roleService.findOne(6);
+	}
+	
+	@PostMapping("roles") // localhost:8080/roles methode=post => http : corps de la requete 
+	// @RequestMapping(value="roles", method=RequestMethod.POST)
+	public Role saveRole(@RequestBody Role role) { // recupere les données en utilisant le body de la requete 
+		return roleService.save(role); 
+	}
+	
+	@DeleteMapping("roles/{idRole}")
+	//@RequestMapping(value="roles/{idRole}", method=RequestMethod.DELETE)
+	public void deleteRole(@PathVariable("idRole")Long idRole) {
+		roleService.delete(idRole);
+	}
+	
+	@PutMapping
+	@RequestMapping(value="roles/{idR}", method=RequestMethod.PUT)
+	public Role updateRole(@PathVariable("idR")Long idRole, @RequestBody Role role) {
+		Role currentRole=roleService.findOne(idRole); 
+		currentRole.setLibelle(role.getLibelle());
+		return roleService.save(currentRole);
+	}
 }
--- src/main/java/com/inti/service/interfaces/IAdministrateurService.java
+++ src/main/java/com/inti/service/interfaces/IAdministrateurService.java
@@ -3,14 +3,31 @@ package com.inti.service.interfaces;
 import java.util.List;
 
 import com.inti.entities.Administrateur;
+import com.inti.entities.Chauffeur;
+import com.inti.entities.Client;
+import com.inti.entities.ResponsableAgence;
+import com.inti.entities.Utilisateur;
 
 public interface IAdministrateurService {
-	List<Administrateur> findAll();
-
-	Administrateur findOne(Long idAdministrateur);
-
-	Administrateur save(Administrateur admin);
-
-	void delete(Long idAdministrateur);
+	List<Administrateur> findAllAdmin();
+	Administrateur findOneAdmin(Long idAdmin);
+	Administrateur saveAdmin(Administrateur admin);
+	void deleteAdmin(Long idAdmin);
+	
+	List<ResponsableAgence> findAllRespo();
+	ResponsableAgence findOneRespo(Long idRespo);
+	ResponsableAgence saveRespo(ResponsableAgence respo);
+	void deleteRespo(Long idRespo);
+	
+	List<Client> findAllClient();
+	Client findOneClient(Long idClient);
+	Client saveClient(Client client);
+	void deleteClient(Long idClient);
+	
+	List<Chauffeur> findAllChauffeur();
+	Chauffeur findOneChauffeur(Long idChauffeur);
+	Chauffeur saveChauffeur(Chauffeur chauffeur);
+	void deleteChauffeur(Long idChauffeur);
+	
 }
 
